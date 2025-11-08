5e1941bcbb4962892a0067c29837374d956ed30c
kpuentec <kcortez1130@gmail.com>
2025-11-07 22:36:47 -0500

    config changes

--- app/config.py
+++ app/config.py
@@ -1,8 +1,16 @@
 import os
 from dotenv import load_dotenv
 
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY not set in environment")
+
+
+
+'''
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY not set in .env")
 MONGO_URI = os.getenv("MONGO_URI")
+'''
