016b1e7bf0710a0f70baddf8918ffa36977834df
kpuentec <kcortez1130@gmail.com>
2025-11-07 22:38:25 -0500

    config changes again

--- app/config.py
+++ app/config.py
@@ -5,6 +5,9 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY not set in environment")
 
+MONGO_URI = os.environ.get("MONGO_URI")
+if not MONGO_URI:
+    raise ValueError("MONGO_URI not set in environment")
 
 
 '''
