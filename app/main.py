285d64311e498cbfc85c501d33eba9700fda1ff7
kpuentec <kcortez1130@gmail.com>
2025-11-23 20:32:56 -0500

    keepalive

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import analyze
 from datetime import datetime, timezone
-
+from keepalive import keep_alive
 app = FastAPI(title="AI Threat Reporter API", version="1.0")
-
+#keep_alive()
 # enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
